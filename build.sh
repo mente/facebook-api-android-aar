9e43189efd328708f396d1e7d68d172686466101
Alex Vasilenko <aa.vasilenko@gmail.com>
2014-05-22 11:09:23 +0300

    updated sdk to 3.14.1 do not build javadoc as it fails

--- build.sh
+++ build.sh
@@ -7,12 +7,11 @@ then
 fi  
 
 version=$1
-./gradlew clean sources javadocJarRelease && git checkout gh-pages && mvn install:install-file \
+./gradlew clean sources && git checkout gh-pages && mvn install:install-file \
 	-Dfile=build/libs/facebook-api-android-aar.aar\
 	-DgroupId=com.facebook\
 	-DartifactId=facebook-android-sdk\
 	-Dversion=$version\
 	-Dpackaging=aar\
 	-Dsources=build/libs/facebook-api-android-aar-sources.jar\
-	-Djavadoc=build/libs/facebook-api-android-aar-javadoc.jar\
 	-DlocalRepositoryPath=./
