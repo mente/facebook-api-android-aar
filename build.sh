0355e2332105bc0c7f17a5911e619527fd2c31e9
Alex Vasilenko <aa.vasilenko@gmail.com>
2014-10-08 12:46:54 +0300

    fixed build script

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ fi
 
 version=$1
 ./gradlew clean sources && git checkout gh-pages && mvn install:install-file \
-	-Dfile=build/outputs/aar/facebook-api-android-aar.aar\
+	-Dfile=build/libs/facebook-api-android-aar.aar\
 	-DgroupId=com.facebook\
 	-DartifactId=facebook-android-sdk\
 	-Dversion=$version\
